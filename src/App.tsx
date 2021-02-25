68270093b7f8bf17d59a0b421db446468dd3aca3
Cedric Manamela <manamelacedric@gmail.com>
2021-02-26 00:06:55 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  font-family: 'Poppins', sans-serif;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+  )
 }
 
-export default App;
+export default App
